d5c1f0cc4788275c50f6a37242d48bc96d9a9676
prrzeemoo <przemekstopka@gmail.com>
2017-07-30 17:24:53 +0200

    NAJNOWSZY

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Category;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,9 +14,10 @@ class CategoryController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $categories = $this->getDoctrine()->getRepository('Appbundle:Category')->findAll();
+        // Render Template
         return $this->render('category/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+            'categories' => $categories
         ]);
     }
 
